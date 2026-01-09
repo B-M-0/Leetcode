e37b2e50032d7335009fb55e05124b94112a58ab
Abdul Mahdi <abdulmahdi1856@gmail.com>
2026-01-09 11:31:42 +0000

    Sync LeetCode submission Runtime - 4 ms (42.56%), Memory - 44.6 MB (29.34%)

---
+++ solutions/0120-triangle/solution.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int MinimumTotal(IList<IList<int>> triangle) {
+    
+        for(int i = triangle.Count() -1; i > 0; i--)
+        {
+            for(int j = 0 ; j < triangle[i-1].Count() ;j++ )
+                triangle[i-1][j] += Math.Min(triangle[i][j],triangle[i][j+1]);
+        }
+        return triangle[0][0];
+    }
+}
